50168e35b913aac29c5e45b3c4b8a320326ca18c
Eden-Eltume <IEltume@gmail.com>
2017-01-09 12:53:37 -0500

    Arrays

---
+++ 7_Arrays/6_error.rb
@@ -0,0 +1,3 @@
+# arrays are indexed with integers, not strings
+# You do this instead
+arr[3] = "Jody"
---
+++ 7_Arrays/map_and_collect.rb
@@ -0,0 +1,3 @@
+a = [1,2,3,4]
+puts a.map { |num| num ** 2 }
+puts a.collect { |num| num ** 2 }
