cd2979efbfe2ee5025630fdf8ce6edd0808d18b5
Eden Eltume <IEltume@gmail.com>
2018-01-05 01:35:38 -0500

    finished Methods 3

--- Introduction_to_Programming_with_Ruby/3_Variables3/ex5.rb
+++ Introduction_to_Programming_with_Ruby/3_Variables3/ex5.rb
@@ -1,4 +1,4 @@
 =begin
-In the first case x prints to the screen the number 6
+In the first case x prints to the screen the number 3
 In the second case x prints to the screen "undefined local variable"
 =end
