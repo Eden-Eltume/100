e6eae86ae5c2adfe084b2e3ec29dac03e394213c
Eden Eltume <IEltume@gmail.com>
2018-09-22 10:50:10 -0400

    Loops 2

---
+++ 100_Exercises/Ruby_Basics/4_Loops_1/ex1.rb
@@ -0,0 +1,6 @@
+counter = 0
+loop do
+  puts 'Just keep printing...'
+  counter += 1
+  break if counter == 1
+end
---
+++ 100_Exercises/Ruby_Basics/4_Loops_1/ex6.rb
@@ -0,0 +1,8 @@
+numbers = []
+
+while numbers.size < 5
+  random_number = rand(100)
+  numbers << random_number
+end
+
+puts numbers
