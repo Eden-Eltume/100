295d512f94a89016e9abd92c866c68310c82e4dc
Eden Eltume <IEltume@gmail.com>
2018-09-23 09:28:50 -0400

    Conditionals

---
+++ 100_Exercises/Ruby_Basics/9_Conditionals/ex5.rb
@@ -0,0 +1,1 @@
+# "My favorite number is 7." This is beacause ruby expressions evaluate to true when used in a condition with the exception of false and nil. 
---
+++ 100_Exercises/Ruby_Basics/9_Conditionals/ex7.rb
@@ -0,0 +1,10 @@
+stoplight = ['green', 'yellow', 'red'].sample
+
+if stoplight == "red"
+	puts "Stop!"
+elsif stoplight == "yellow"
+	puts "Slow down!"
+else
+	puts "Go!"
+end
+		
